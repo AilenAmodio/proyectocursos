3299b5eb02eb344c0222f050f7e55de5a9fd1924
ailen <ailen@DESKTOP-25R8SJB>
2021-04-08 15:06:49 -0300

    se agrega alumno a la lista

--- curs/src/main/java/proyecto/cursos/curso/controller/AlumnoController.java
+++ curs/src/main/java/proyecto/cursos/curso/controller/AlumnoController.java
@@ -1,23 +1,13 @@
 package proyecto.cursos.curso.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.expression.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import proyecto.cursos.curso.entidades.Alumno;
-import proyecto.cursos.curso.entidades.Curs;
 import proyecto.cursos.curso.repo.AlumnoRepository;
 
 @Controller
@@ -34,11 +24,12 @@ public class AlumnoController {
 		}
 	
 	@RequestMapping(value = "/save", method = { RequestMethod.POST, RequestMethod.PUT })
-	public String save(@RequestParam(value = "nombre") String nombre, @RequestParam(value = "apellido") String apellido,
-		@RequestParam(value = "dni") Long dni,
-		Model model) throws ParseException {
-		Optional<Alumno> alumno = alumnoRepo.findByNombre(nombre);;
+	public String save (@RequestParam(value = "nombre") String nombre, @RequestParam(value = "apellido") String apellido,
+			@RequestParam(value = "dni") Long dni, Model model) 
+		throws ParseException {
+		Alumno alumno = new Alumno(nombre, apellido, dni);
 		model.addAttribute("alumno", alumnoRepo.findByNombre(nombre));
+		alumnoRepo.save(alumno);
 		return "redirect:/listado";
 	}
 	
